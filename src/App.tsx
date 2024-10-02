78169defe8e3a2d76efdd166fba54644dc225cbe
Semin Bae <semin.bae.1999@gmail.com>
2024-10-02 23:37:28 +0900

    (1002) connect App.tsx with basic templates

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,29 @@ import Navbar from './component/0.Navbar'
 import './App.css'
 
 function App() {
-
-  return (
-  )
+  const Navbar: React.FC = () => {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/services">Services</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+          <li>
+            <a href="/faq">FAQ</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
 }
 
 export default App
