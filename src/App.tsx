64e8266dcda5bab8ca58ae9a9e63ad74fd675775
Semin Bae <semin.bae.1999@gmail.com>
2024-10-03 00:43:37 +0900

    (1003) cont... work for nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
-import { useState } from 'react'
-import Navbar from './component/0.Navbar'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './component/0.Navbar';
+import Home from './component/1.Home';
+import FacilityList from './component/2.FacilityList';
+import FacilityReservation from './component/3.FacilityReservation';
+import MyInformation from './component/4.MyInformation';
+import MyReservation from './component/5.MyReservation';
+import './App.css';
 
-function App() {
-  const Navbar: React.FC = () => {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/services">Services</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-          <li>
-            <a href="/faq">FAQ</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  };
-}
+const App: React.FC = () => {
+  return (
+    <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/facilityList" element={<FacilityList />} />
+      <Route path="/facilityReservation" element={<FacilityReservation />} />
+      <Route path="/myInformation" element={<MyInformation />} />
+      <Route path="/myReservation" element={<MyReservation />} />
+    </Routes>
+  </Router>
+  );
+};
 
-export default App
+export default App;
